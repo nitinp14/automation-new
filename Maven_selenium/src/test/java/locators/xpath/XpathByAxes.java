4b744cc0b03db73de7fbd4463a892fb694e7310a
Nitin <Nitin@DESKTOP-JHMLI0H.bbrouter>
2023-06-21 20:35:25 +0530

    21-06-23

--- Maven_selenium/src/test/java/locators/xpath/XpathByAxes.java
+++ Maven_selenium/src/test/java/locators/xpath/XpathByAxes.java
@@ -1,7 +1,10 @@
 package locators.xpath;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class XpathByAxes {
 	public static void main(String[] args) {
-		
+		WebDriver driver = new ChromeDriver();
 	}
 }
